3265637654e4737679f2b1f2eb2070adae13bab1
xmkjw63 <mzxmc@qq.com>
2023-06-30 13:59:34 +0800

    modified:   routes/manager.py

--- routes/manager.py
+++ routes/manager.py
@@ -4,10 +4,9 @@ from database import get_connection
 
 router = APIRouter()
 
-@router.post("/apis/add")
-async def add_anime(anime_url: str, conn=Depends(get_connection)):
+##async def add_anime(anime_url: str, conn=Depends(get_connection)):
 #添加至数据库
 
-@router.delete("/apis/delete/{anime_id}")
-def delete(anime_id: int, conn=Depends(get_connection)):
+#@router.delete("/apis/delete/{anime_id}")
+#def delete(anime_id: int, conn=Depends(get_connection)):
 #去数据库删除链接
